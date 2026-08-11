c24ef0098fac767c0396a9fdc0e0a178b4f862b1
NarayaN Singh <narayan.singh.2571989@gmail.com>
2026-08-11 16:24:39 +0530

    chore: update WhatsApp community invite link (#8)

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,4 @@
 // Global site configuration and social community links
-export const WHATSAPP_COMMUNITY_URL = "https://chat.whatsapp.com/COMMUNITY_JOIN_LINK";
+export const WHATSAPP_COMMUNITY_URL = "https://chat.whatsapp.com/CloTTI8qWW286dzvlbGAZW?s=cl&p=i&ilr=2";
 export const SITE_PHONE = "+91 98765 43210";
 export const SITE_EMAIL = "info@plus530adventure.com";
